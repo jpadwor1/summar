c6a2cda65ad09d9bde0399753c7b606b8f394a68
jpadwor1 <128070765+jpadwor1@users.noreply.github.com>
2023-12-05 18:03:08 -0800

    chore: update .env

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,68 @@
-import Link from "next/link";
-import MaxWidthWrapper from "./MaxWidthWrapper";
-import { buttonVariants } from "./ui/button";
+import Link from 'next/link';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { buttonVariants } from './ui/button';
 import {
   LoginLink,
   RegisterLink,
   LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/server";
-import { ArrowRight } from "lucide-react";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+} from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import UserAccountNav from './UserAccountNav';
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-flow border-b border-gray-200 bg-white/75 backdrop-blur-lg transtion-all">
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-flow border-b border-gray-200 bg-white/75 backdrop-blur-lg transtion-all'>
       <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href="/" className="flex z-40 font-semibold">
-            <span className="text-2xl text-zinc-700">Summan.</span>
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link href='/' className='flex z-40 font-semibold'>
+            <span className='text-2xl text-zinc-700'>Summan.</span>
           </Link>
 
           {/* TODO: add mobile navbar */}
-          <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({ variant: "ghost", size: "sm" })}
-              >
-                Pricing
-              </Link>
-              {user?.id ? (
-                <LogoutLink
-                  className={buttonVariants({ variant: "ghost", size: "sm" })}
+          <div className='hidden items-center space-x-4 sm:flex'>
+            {!user ? (
+              <>
+                <Link
+                  href='/pricing'
+                  className={buttonVariants({ variant: 'ghost', size: 'sm' })}
                 >
-                  Logout
-                </LogoutLink>
-              ) : (
+                  Pricing
+                </Link>
+
                 <LoginLink
-                  className={buttonVariants({ variant: "ghost", size: "sm" })}
+                  className={buttonVariants({ variant: 'ghost', size: 'sm' })}
                 >
                   Login
                 </LoginLink>
-              )}
-              <RegisterLink className={buttonVariants({ size: "sm" })}>
-                Get Started <ArrowRight className="ml-1.5 h-5 w-5" />
-              </RegisterLink>
-            </>
+
+                <RegisterLink className={buttonVariants({ size: 'sm' })}>
+                  Get Started <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href='/Dashboard'
+                  className={buttonVariants({ variant: 'ghost', size: 'sm' })}
+                >
+                  Dashboard
+                </Link>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  imageUrl={user.picture ?? ''}
+                  email={user.email ?? ''}
+                />
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
